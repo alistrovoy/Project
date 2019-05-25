8c3a7b283834d9a8509cc54c3ee227bd0ae91642
alistrovoy <alexanderlistrovoy@gmail.com>
2019-05-25 12:19:45 +0300

    !

---
+++ src/homeworkMatrix/Matrix.java
@@ -0,0 +1,27 @@
+package homeworkMatrix;
+
+public class Matrix {
+
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] a = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = n * i + j;
+            }
+        }
+
+
+        System.out.println(("Матрица"));
+        System.out.println("------");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%4d", a[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
